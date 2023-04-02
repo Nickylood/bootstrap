38efc503a3ea5582ad6823fea06392f8f1474cf7
Nickylood <110985074+Nickylood@users.noreply.github.com>
2023-04-02 14:59:05 +0200

    next

--- bootstrap/test-filter/main.js
+++ bootstrap/test-filter/main.js
@@ -1,106 +1,106 @@
 fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => data(json));
+  .then(response => response.json())
+  .then(json => data(json));
 
-      let gData = undefined;
+let gData = undefined;
 
 function data(json) {
-    gData = json = json.map(x => {
-        delete x["address"];
-        delete x["company"];
-        return x;
-    });
-    getTableHeader(Object.keys(json[0]));
-    getTableData(json);
+  gData = json = json.map(x => {
+    delete x["address"];
+    delete x["company"];
+    return x;
+  });
+  getTableHeader(Object.keys(json[0]));
+  getTableData(json);
 };
 
 function getTableHeader(fields) {
-    const table = document.querySelector('.table');
-    const thead = document.createElement('thead');
-    const tr = document.createElement('tr');
-    const fragment = document.createDocumentFragment();
-    fields.forEach(x => {
-        const th = document.createElement('th');
-        th.innerText = x;
-        th.scope = "col";
-        th.id = x;
-        fragment.appendChild(th);
-    });
-    tr.appendChild(fragment);
-    thead.appendChild(tr);
-    table.appendChild(thead);
-    clickFilterEvents(fields);
+  const table = document.querySelector('.table');
+  const thead = document.createElement('thead');
+  const tr = document.createElement('tr');
+  const fragment = document.createDocumentFragment();
+  fields.forEach(x => {
+    const th = document.createElement('th');
+    th.innerText = x;
+    th.scope = "col";
+    th.id = x;
+    fragment.appendChild(th);
+  });
+  tr.appendChild(fragment);
+  thead.appendChild(tr);
+  table.appendChild(thead);
+  clickFilterEvents(fields);
 };
 
 function getTableData(data) {
-    const table = document.querySelector('.table');
-    const tbody = document.createElement('tbody');
-    data.forEach(x => {
-        const tr = document.createElement('tr');
-        const fragment = document.createDocumentFragment();
-        const keys = Object.keys(data[0]);
-        keys.forEach(y => {
-            let td = undefined;
-            if (y === "id") {
-                td = document.createElement('th');
-                td.scope ="row";
-            } else {
-                td = document.createElement('td');
-            }
-            td.innerText = x[y];
-            fragment.appendChild(td);
-        });
-        tr.appendChild(fragment);
-        tbody.appendChild(tr);
-        table.appendChild(tbody);
+  const table = document.querySelector('.table');
+  const tbody = document.createElement('tbody');
+  data.forEach(x => {
+    const tr = document.createElement('tr');
+    const fragment = document.createDocumentFragment();
+    const keys = Object.keys(data[0]);
+    keys.forEach(y => {
+      let td = undefined;
+      if (y === "id") {
+        td = document.createElement('th');
+        td.scope ="row";
+      } else {
+        td = document.createElement('td');
+        td.setAttribute('contenteditable', true);
+      }
+      td.innerText = x[y];
+      fragment.appendChild(td);
     });
+    tr.appendChild(fragment);
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
+  });
+  replacement();
 };
 
 function deleteTableData() {
-    const tbody = document.querySelector('.table tbody');
-    tbody.remove();
+  const tbody = document.querySelector('.table tbody');
+  tbody.remove();
 };
 
 function clickFilterEvents (fields) {
-    fields.forEach(x => {
-        const thisTh = document.querySelector(`#${x}`);
-        const trigsort = sort();
-        thisTh.addEventListener('click', e => {
-            deleteTableData();
-            trigsort(e);
-        });
+  fields.forEach(x => {
+    const thisTh = document.querySelector(`#${x}`);
+    const trigsort = sort();
+    thisTh.addEventListener('click', e => {
+      deleteTableData();
+      trigsort(e);
     });
+  });
 };
 
 function sort () {
-    let trigger = 1;
-    return e => {
-        trigger = -trigger;
-        gData.sort((a, b) => {
-            if(a[e.target.id] > b[e.target.id]) {
-                return trigger;
-            } else {
-                return -trigger;
-            }
-        });
-        getTableData(gData);
-    };
+  let trigger = 1;
+  return e => {
+    trigger = -trigger;
+    gData.sort((a, b) => {
+      if(a[e.target.id] > b[e.target.id]) {
+        return trigger;
+      } else {
+        return -trigger;
+      }
+    });
+    getTableData(gData);
+  };
 };
 
-for (let i = 0; i < td.length; i++) {
-    td[i].addEventListener('click', function func() {
-        let input = document.createElement('input');
-        input.value = td[i].innerHTML;
-        this.innerHTML = '';
-        this.appendChild(input);
-
-        let td = this;
-        input.addEventListener('blur', function() {
-            td.innerHTML = this.value;
-            td.addEventListener('click', func);
-        });
-
-        this.removeEventListener('click', func);
+function replacement() {
+  const tds = document.querySelectorAll('td[contenteditable="true"]');
+  tds.forEach(td => {
+    td.addEventListener('focus', function() {
+      td.setAttribute('data-before', td.innerHTML);
     });
+    td.addEventListener('blur', function() {
+      if (td.getAttribute('data-before') !== td.innerHTML) {
+        const rowIndex = td.parentNode.rowIndex;
+        const cellIndex = td.cellIndex;
+        gData[rowIndex - 1][td.parentNode.parentNode.querySelector('thead th:nth-child(' + (cellIndex + 1) + ')').id] = td.innerHTML;
+      }
+    });
+  });
 }
-
